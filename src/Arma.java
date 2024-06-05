7db9ed377d04635d3205b8cd6e9e53dd52972621
Toritou <l.toro05@ufromail.cl>
2024-06-04 20:31:07 -0400

    clase arma lista

--- src/Arma.java
+++ src/Arma.java
@@ -1,2 +1,18 @@
 public class Arma {
+    //Atributos
+    private String nombre;
+    private int poderDeAtaque;
+    private String tipoDeDanio;
+
+    //Constructor
+    public Arma(String nombre, int poderDeAtaque, String tipoDeDanio) {
+        this.nombre = nombre;
+        this.poderDeAtaque = poderDeAtaque;
+        this.tipoDeDanio = tipoDeDanio;
+    }
+
+    //Metodos
+    public int getPoderAtaque() {
+        return poderDeAtaque;
+    }
 }
